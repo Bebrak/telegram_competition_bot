c83f85d58162be6f803728c2d60a67315cdc4035
BorkinEugen <borkin.zhenya.15@gmail.com>
2023-03-21 20:43:12 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 import config
 from telebot import types
+import random
 
 # telegram - "https://t.me/competitionAssistant_bot"
 # username - @competitionAssistant_bot
@@ -12,20 +13,25 @@ def start(message):
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton('❕ Меню ❕', callback_data='menustart')
     markup.add(item1)
-    bot.send_message(message.chat.id, "Здравствуйте, {0.first_name}."
-                                      "\nЯ - бот ассистент, созданный для улучшения работы <a href='http://айтикуб.рф/'>It-Куба</a>."
-                     .format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
+    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Меню")
+    item2 = types.KeyboardButton("Список команд")
+    markup1.add(item1, item2)
+    bot.send_message(message.chat.id, "Здравствуйте, {0.first_name}.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup1)
+    bot.send_message(message.chat.id,"Я - бот ассистент, созданный для улучшения работы <a href='http://айтикуб.рф/'>It-Куба</a>.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
 
 @bot.message_handler(commands=['menustart'])
 def menustart(message):
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton("📜 Список команд 📜", callback_data='list_of_commands')
-    item3 = types.InlineKeyboardButton('It-Куб Узнать больше', callback_data='itcube')
-    item2 = types.InlineKeyboardButton('It-Куб в вашем городе', callback_data='yourcity')
-    item4 = types.InlineKeyboardButton('Информация о боте', callback_data='author')
-    markup.add(item1, item2, item3, item4)
+    item2 = types.InlineKeyboardButton('It-Куб Новости', url='http://айтикуб.рф/news091121')
+    item4 = types.InlineKeyboardButton('It-Куб Узнать больше', callback_data='itcube')
+    item3 = types.InlineKeyboardButton('It-Куб в вашем городе', callback_data='yourcity')
+    item5 = types.InlineKeyboardButton('Информация о боте', callback_data='author')
+    markup.add(item1, item2, item3, item4, item5)
     bot.send_message(message.chat.id, '<b>Главное меню</b>'
                                       '\n● Список команд - выводит на экран все доступные команды.'
+                                      '\n● It-куб Новости - показывает последние новости It-Куб.'
                                       '\n● It-Куб в вашем городе - помогает найти центр It-Куб в вашем городе.'
                                       '\n● It-Куб Узнать больше - выводит подробную информацию о It-Куб.'
                                       '\n● Информация о боте - выводит информацию о боте.', reply_markup=markup, parse_mode='html')
@@ -33,15 +39,31 @@ def menustart(message):
 def menu(message):
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton("📜 Список команд 📜", callback_data='list_of_commands')
-    item3 = types.InlineKeyboardButton('It-Куб Узнать больше', callback_data='itcube')
-    item2 = types.InlineKeyboardButton('It-Куб в вашем городе', callback_data='yourcity')
-    item4 = types.InlineKeyboardButton('Информация о боте', callback_data='author')
-    markup.add(item1, item2, item3, item4)
+    item2 = types.InlineKeyboardButton('It-Куб Новости', url='http://айтикуб.рф/news091121')
+    item4 = types.InlineKeyboardButton('It-Куб Узнать больше', callback_data='itcube')
+    item3 = types.InlineKeyboardButton('It-Куб в вашем городе', callback_data='yourcity')
+    item5 = types.InlineKeyboardButton('Информация о боте', callback_data='author')
+    markup.add(item1, item2, item3, item4, item5)
     bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text='<b>Главное меню</b>'
                                       '\n● Список команд - выводит на экран все доступные команды.'
+                                      '\n● It-куб Новости - показывает последние новости It-Куб.'
                                       '\n● It-Куб в вашем городе - помогает найти центр It-Куб в вашем городе.'
                                       '\n● It-Куб Узнать больше - выводит подробную информацию о It-Куб.'
                                       '\n● Информация о боте - выводит информацию о боте.', reply_markup=markup, parse_mode='html')
+
+@bot.message_handler(commands=['author'])
+def author(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    item1 = types.InlineKeyboardButton('⛔ Назад ⛔', callback_data='menu')
+    markup.add(item1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text='<b>Информация о боте</b>'
+                                      '\nБота создал - Боркин Евгений для конкурса <a href="http://ит-куб-пенза.ит-колледж.рф/час-земли/">"Час земли" от It-Куб Пенза</a>.'
+                                      '\n'
+                                      '\n<a href="https://github.com/Bebrak/telegram_competition_bot.git">github - https://github.com/Bebrak/telegram_competition_bot.git</a>'
+                                      '\n'
+                                      '\n<a href="https://drive.google.com/drive/folders/10cYyyyx1-z_qwfn7DQlWZX8wzuQZS_PO?usp=sharing">Гугл диск - https://drive.google.com/drive/folders/10cYyyyx1-z_qwfn7DQlWZX8wzuQZS_PO?usp=sharing</a>'
+                                      '\n',reply_markup=markup, parse_mode='html')
+
 @bot.message_handler(commands=['itcube'])
 def itcube(message):
     markup = types.InlineKeyboardMarkup(row_width=1)
@@ -51,8 +73,9 @@ def itcube(message):
     item4 = types.InlineKeyboardButton('⛔ Назад ⛔', callback_data='menu')
     markup.add(item1, item2, item3, item4)
     bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text='<b>It-Куб</b>'
-                                      '\nIt-Куб - это центры цифрового образования детей по всей России.'
-                                      '\nНа данный момент в программе доступно множество направлений, такие как "Мобильная разработка" и "Программирование роботов".'
+                                      '\nЭто центр образования детей по программам, направленным на ускоренное освоение актуальных и востребованных знаний, навыков и компетенций в сфере информационных технологий.'
+                                      '\nПроект формирует современную образовательную экосистему, объединяющую компании-лидеров ИТ-рынка, опытных наставников и начинающих разработчиков от 7 до 18 лет.'
+                                      '\nНа данный момент в программе доступно множество направлений, таких как "Мобильная разработка" и "Программирование роботов".'
                                       '\nЭту и другую информацию вы найдете на официальном сайте It-Куб и в группе ВК.',reply_markup=markup, parse_mode='html')
 
 @bot.message_handler(commands=['yourcity'])
@@ -72,19 +95,37 @@ def yourcity(message):
                                       '\nСейчас в Российской Федерации находится более 150 центров It-Куб.'
                                       '\nПоэтому ниже перечислены лишь некоторые города.', reply_markup=markup, parse_mode='html')
 
+@bot.message_handler(commands=['list_of_commands'])
+def list_of_commands(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    item1 = types.InlineKeyboardButton('⛔ Назад ⛔', callback_data='menu')
+    markup.add(item1)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text='<b>Список команд</b>'
+                                      '\n● /menustart - перейти в меню.'
+                                      '\n● /start - перейти на приветственный экран.'
+                                      '\n● /yourcity - искать центр It-Куб в вашем городе.'
+                                      '\n● /itcube - узнать больше о It-Куб.'
+                                      '\n● /author - узнать больше о боте.', reply_markup=markup, parse_mode='html')
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+    if message.chat.type == 'private':
+        if message.text == 'Меню':
+            menustart(message)
+        elif message.text == 'Список команд':
+            list_of_commands(message)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     try:
         if call.message:
             if call.data == 'list_of_commands':
-                pass
+                list_of_commands(call.message)
             elif call.data == 'menu':
                 menu(call.message)
             elif call.data == 'itcube':
                 itcube(call.message)
             elif call.data == 'author':
-                pass
+                author(call.message)
             elif call.data == 'yourcity':
                 yourcity(call.message)
             elif call.data == 'menustart':
